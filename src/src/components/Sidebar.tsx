26c62eaf972bed3264d4d478f35866902ea36448
Nagaraj L <nagarajnainar8458@gmail.com>
2025-12-02 00:11:18 +0530

    Returns issue fixed

--- src/src/components/Sidebar.tsx
+++ src/src/components/Sidebar.tsx
@@ -219,7 +219,7 @@ export const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
                   }
                 >
                   <Undo2 className="w-5 h-5" />
-                  <span className="flex-1">Purchase Returns</span>
+                  <span className="flex-1">Returns</span>
                 </NavLink>
                 <NavLink
                   to="/scales-screen"
